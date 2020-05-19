baac86a024098c365a6d2b81b035409fcc640983
espartacocamero <espartaco.camero@gmail.com>
2020-05-19 17:46:35 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will tell you where to invest your money according to your prefered stocks',
+    author='Espartaco Camero',
+    license='',
+)
