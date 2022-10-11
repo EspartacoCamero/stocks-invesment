e879f6e0383b3fe84155bc46fdb5e87d82493bc3
espartacocamero <espartaco.camero@gmail.com>
2022-10-12 00:08:01 +0200

    fea: telebot class test

--- src/utils/telebot.py
+++ src/utils/telebot.py
@@ -1,11 +1,19 @@
 import requests
 
 class Tabot:
-    def __init__(self, chat_token, chat_id):
-        BOT_URL = 'https://api.telegram.org/bot'
+    BOT_URL = 'https://api.telegram.org/bot'
 
+    def __init__(self, chat_token, chat_id):
         self.chat_token = chat_token
         self.chat_id = chat_id
 
     def __str__(self):
         return "Tabot with chat token: {token} and chat_id {chat_id}".format(token=self.chat_token, chat_id=self.chat_id)
+
+    def get_chat_id(self):
+        url = Tabot.BOT_URL + str(self.chat_token) + '/getUpdates'
+        #response = requests.get('https://api.telegram.org/bot5675115970:AAFxftIMTUwBrJtOMRfPZjI3e3_3kFy0YKU/getUpdates')
+        response = requests.get(url)
+        print(url)
+
+        return response.json()['result'][0]['message']['chat']['id']
