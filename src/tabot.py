afd1bfd86a320931977f5e42d9e4e51b6f0523df
espartacocamero <espartaco.camero@gmail.com>
2022-10-17 22:09:16 +0200

    fea: logging module + using os for path

--- src/tabot.py
+++ src/tabot.py
@@ -7,11 +7,17 @@ from pandas_datareader import data as pdr
 import yfinance as yfin
 yfin.pdr_override()
 import yaml
-from utils import utils, telebot
+from utils import utils, telebot, logger_utils
 import os
 
-all_config = yaml.safe_load(open("config_file.yml", "r"))
-df_input = utils.get_ticks_data('src/data/example_data.csv')
+FILE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+t0 = dt.datetime.now()
+all_config = yaml.safe_load(open(os.path.join(FILE_PATH, "../config_file.yml"), "r"))
+logger = logger_utils.log_creator(all_config['output']['log_folder'], log_name='tabot')
+
+logger.info("Reading tick data")
+df_input = utils.get_ticks_data(os.path.join(FILE_PATH, 'data/example_data.csv'))
 
 start_date, end_date = utils.get_start_end(all_config)
 source = all_config['stocks']['source']
@@ -19,7 +25,7 @@ metric = all_config['stocks']['metric']
 ticks = df_input['TICK'].to_list()
 
 df = utils.get_stock_data(ticks, start_date, end_date, col=metric)
-print("Following ticks will be analyzed: \n %s" % df.columns)
+logger.info("Following ticks will be analyzed: \n %s" % df.columns)
 
 df_clean = utils.get_metrics(df, all_config['model']['kpis'])
 dflags = utils.get_flags(df_clean, all_config['strategy'])
@@ -40,7 +46,7 @@ msg_header = '\nTICK \t PRICE RSI\n'
 
 msgs = []
 for row in dflags.itertuples():
-    msgs.append('{s} \t {p} \t {rsi} \n'.                format(s=row.stock, p=np.round(row.price,1), rsi=np.round(row.rsi, 1), rsi_f=row.rsi_f,bb_f= row.bb_f, ema_f=row.ema_f, score= row.score))
+    msgs.append('{s} \t {p} \t {rsi} \n'.format(s=row.stock, p=np.round(row.price,1), rsi=np.round(row.rsi, 1)))
 
 msg_formated = ' '.join(msgs)
 message = msg_title + msg_header + msg_formated
